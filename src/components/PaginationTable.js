b1f19894f30311139d3bbaf545f7a15355df1154
Gregfanyan <grigorfanyan@gmail.com>
2021-04-20 15:48:30 +0200

    added initial values for pagination, user can jump to any page

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -20,10 +20,13 @@ const PaginationTable = () => {
     canPreviousPage,
     pageOptions,
     state,
+    gotoPage,
+    pageCount,
   } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 2 },
     },
     usePagination
   );
@@ -69,11 +72,31 @@ const PaginationTable = () => {
             {pageIndex + 1} of {pageOptions.length}
           </strong>{" "}
         </span>
+        <span>
+          | Go to Page :{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previous
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
         </button>
       </div>
     </>
