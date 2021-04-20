8017922b43e34d36bb904f6fe1254bb7c079a0c2
Gregfanyan <grigorfanyan@gmail.com>
2021-04-20 15:05:51 +0200

    debauncing filter functionality for run time

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAsyncDebounce } from "react-table";
 
 const GlobalFilter = ({ filter, setFilter }) => {
+  const [value, setValue] = useState(filter);
+
+  const onChange = useAsyncDebounce(
+    (value) => setFilter(value || undefined),
+    300
+  );
+
   return (
     <span>
       Search:{" "}
-      <input value={filter || ""} onChange={(e) => setFilter(e.target.value)} />
+      <input
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
     </span>
   );
 };
